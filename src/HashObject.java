8247569fa36eaa85bd997e91706187ec2a3ad176
akkaneko <akkstuff@thacher.org>
2015-02-04 23:29:59 -0800

    Entered hash function

--- src/HashObject.java
+++ src/HashObject.java
@@ -169,7 +169,8 @@ public class HashObject {
 	 * 
 	 ******************************************************************/
 	private int customRehasherAK(int oldIndex) {
-		return 0;
+		quadIncrement += 11;
+		return (oldIndex + (quadIncrement)) % capacity;
 	}
 	/******************************************************************
 	 * custom rehasher implements ? ? ? ? ? ? ? your design
